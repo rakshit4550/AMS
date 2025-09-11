e5a97feb439d7db09772a69b2402c4cf2796cd7d
jay <jayradadiya1694@gmail.com>
2025-09-11 17:13:12 +0530

    add change slice

--- client/src/redux/partySlice.jsx
+++ client/src/redux/partySlice.jsx
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = process.env.REACT_APP_API_URL + '/parties'
 
 // Async thunks for API calls
 export const fetchParties = createAsyncThunk('party/fetchParties', async () => {
