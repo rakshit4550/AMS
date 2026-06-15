c8593e8e91e8ec48d6b41c8b843830e24ddaa5df
rakshit4550 <rakshitramani99@gmail.com>
2026-06-15 09:52:21 +0530

    remove comment code

--- server/model/Party.js
+++ server/model/Party.js
@@ -1,25 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const partySchema = new mongoose.Schema({
-//   partyname: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-//   createdBy: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true,
-//   },
-// }, {
-//   timestamps: true,
-// });
-
-// const Party = mongoose.model('Party', partySchema);
-
-// export default Party;
-
-
 import mongoose from 'mongoose';
 
 const partySchema = new mongoose.Schema({
